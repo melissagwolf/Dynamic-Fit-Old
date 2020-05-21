ced3d05d41857601baee4561a6786963bf45adcd
melissagwolf <47953444+melissagwolf@users.noreply.github.com>
2020-05-20 23:47:50 -0700

    Initial copy from computer

---
+++ Deploy App.R
@@ -0,0 +1,5 @@
+library(rsconnect)
+
+rsconnect::setAccountInfo(name='melissagwolf', token='CDF073365AC304DFB687B4A44BDF9790', secret='WW8+SCx0PvWyZ+bDo1uks2L4ywfGt9aD0Xd33Jcf')
+
+rsconnect::deployApp('C:/users/missg/OneDrive/Documents/Dynamic Model Fit/dynamic/WebsiteCopy.Rmd')
